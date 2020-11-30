Fixed no spikes in a bin filtering, saved average firing rates for each unit.

Faraut - Human MTL/faraut_amygdala_all_plots.py
@@ -27,9 +27,11 @@ all_means_amygdala_f = []
 amyg_taus_f = []
 amyg_failed_fits = []
 
-failed_autocorr = []
-no_spikes_in_a_bin = []
-low_fr = []
+f_amyg_failed_autocorr = []
+f_amyg_no_spikes_in_a_bin = []
+f_amyg_low_fr = []
+
+f_amyg_avg_fr = []
 
 for unit in range(len(spikes)):
 
@@ -49,13 +51,15 @@ for unit in range(len(spikes)):
             
         binned_unit_spikes = np.vstack(binned_unit_spikes)
         
+        [trials,bins] = binned_unit_spikes.shape
+        
         summed_spikes_per_bin = np.sum(binned_unit_spikes,axis=0)
+        
+        f_amyg_avg_fr.append(np.sum(summed_spikes_per_bin)/trials)
         
         #%% Do autocorrelation
         
         one_autocorrelation = []
-        
-        [trials,bins] = binned_unit_spikes.shape
         
         for i in range(bins):
             for j in range(bins):
@@ -68,15 +72,15 @@ for unit in range(len(spikes)):
                 
         if np.isnan(one_autocorrelation).any() == True:
             
-            failed_autocorr.append(unit) # skip this unit if any autocorrelation fails
+            f_amyg_failed_autocorr.append(unit) # skip this unit if any autocorrelation fails
         
         elif [summed_spikes_per_bin[bin] == 0 for bin in range(len(summed_spikes_per_bin))]:
             
-            no_spikes_in_a_bin.append(unit) # skip this unit if any bin doesn't have spikes
+            f_amyg_no_spikes_in_a_bin.append(unit) # skip this unit if any bin doesn't have spikes
         
         elif np.sum(summed_spikes_per_bin) < 1:
             
-            low_fr.append(unit) # skip this unit if avg firing rate across all trials is < 1
+            f_amyg_low_fr.append(unit) # skip this unit if avg firing rate across all trials is < 1
         
         else:
                 
@@ -198,7 +202,7 @@ for unit in range(len(spikes)):
         
 #%% How many units got filtered?
 
-bad_units = len(failed_autocorr) + len(no_spikes_in_a_bin) + len(low_fr)
+bad_units = len(f_amyg_failed_autocorr) + len(f_amyg_no_spikes_in_a_bin) + len(f_amyg_low_fr)
 
 print('%i units were filtered out' %bad_units)
 print('out of %i total units' %len(spikes))

Faraut - Human MTL/faraut_hippocampus_all_plots.py
@@ -26,9 +26,11 @@ spikes = hc['spikes']
 all_means_hc_f = []
 hc_taus_f = []
 
-failed_autocorr = []
-no_spikes_in_a_bin = []
-low_fr = []
+f_hc_failed_autocorr = []
+f_hc_no_spikes_in_a_bin = []
+f_hc_low_fr = []
+
+f_hc_avg_fr = []
 
 for unit in range(len(spikes)):
 
@@ -48,13 +50,15 @@ for unit in range(len(spikes)):
             
         binned_unit_spikes = np.vstack(binned_unit_spikes)
         
+        [trials,bins] = binned_unit_spikes.shape
+        
         summed_spikes_per_bin = np.sum(binned_unit_spikes,axis=0)
+        
+        f_hc_avg_fr.append(np.sum(summed_spikes_per_bin)/trials)
         
         #%% Do autocorrelation
         
         one_autocorrelation = []
-        
-        [trials,bins] = binned_unit_spikes.shape
         
         for i in range(bins):
             for j in range(bins):
@@ -67,15 +71,15 @@ for unit in range(len(spikes)):
                 
         if np.isnan(one_autocorrelation).any() == True:
             
-            failed_autocorr.append(unit) # skip this unit if any autocorrelation fails
+            f_hc_failed_autocorr.append(unit) # skip this unit if any autocorrelation fails
         
         elif [summed_spikes_per_bin[bin] == 0 for bin in range(len(summed_spikes_per_bin))]:
             
-            no_spikes_in_a_bin.append(unit) # skip this unit if any bin doesn't have spikes
+            f_hc_no_spikes_in_a_bin.append(unit) # skip this unit if any bin doesn't have spikes
         
         elif np.sum(summed_spikes_per_bin) < 1:
             
-            low_fr.append(unit) # skip this unit if avg firing rate across all trials is < 1
+            f_hc_low_fr.append(unit) # skip this unit if avg firing rate across all trials is < 1
         
         else:
                 
@@ -195,7 +199,7 @@ for unit in range(len(spikes)):
         
 #%% How many units got filtered?
 
-bad_units = len(failed_autocorr) + len(no_spikes_in_a_bin) + len(low_fr)
+bad_units = len(f_hc_failed_autocorr) + len(f_hc_no_spikes_in_a_bin) + len(f_hc_low_fr)
 
 print('%i units were filtered out' %bad_units)
 print('out of %i total units' %len(spikes))

Hunt - monkey frontal cortex/hunt_acc.py
@@ -30,6 +30,8 @@ acc_failed_fits = []
 acc_no_autocorr_monkey = []
 acc_no_spikes_in_a_bin_monkey = []
 acc_low_fr_monkey = []
+
+acc_avg_fr = []
         
 binsize = 50
 
@@ -51,9 +53,10 @@ for unit in range(len(spikes)):
             binned_unit_spikes.append(trial_spikes)
         binned_unit_spikes = np.array(binned_unit_spikes)
         binned_unit_spikes = binned_unit_spikes[:,:-1]
-
         
         summed_spikes_per_bin = np.sum(binned_unit_spikes,axis=0)
+        
+        acc_avg_fr.append(np.sum(summed_spikes_per_bin)/rows)
         
         #%% Do autocorrelation
         
@@ -74,9 +77,9 @@ for unit in range(len(spikes)):
             
             acc_no_autocorr_monkey.append(unit)
             
-        # elif [summed_spikes_per_bin[bin] == 0 for bin in range(len(summed_spikes_per_bin))]:
-            
-        #     acc_no_spikes_in_a_bin_monkey.append(unit)
+        elif np.any(summed_spikes_per_bin[bin] == 0 for bin in range(len(summed_spikes_per_bin))) == True:
+            # If there is not a spike in every bin
+            acc_no_spikes_in_a_bin_monkey.append(unit)
             
         elif np.sum(summed_spikes_per_bin) < 1:
             

Hunt - monkey frontal cortex/hunt_dlpfc.py
@@ -30,6 +30,8 @@ dlpfc_failed_fits = []
 dlpfc_no_autocorr_monkey = []
 dlpfc_no_spikes_in_a_bin_monkey = []
 dlpfc_low_fr_monkey = []
+
+dlpfc_avg_fr = []
         
 binsize = 50
 
@@ -52,8 +54,9 @@ for unit in range(len(spikes)):
         binned_unit_spikes = np.array(binned_unit_spikes)
         binned_unit_spikes = binned_unit_spikes[:,:-1]
 
-        
         summed_spikes_per_bin = np.sum(binned_unit_spikes,axis=0)
+        
+        dlpfc_avg_fr.append(np.sum(summed_spikes_per_bin)/rows)
         
         #%% Do autocorrelation
         
@@ -74,9 +77,9 @@ for unit in range(len(spikes)):
             
             dlpfc_no_autocorr_monkey.append(unit)
             
-        # elif [summed_spikes_per_bin[bin] == 0 for bin in range(len(summed_spikes_per_bin))]:
-            
-        #     dlpfc_no_spikes_in_a_bin_monkey.append(unit)
+        elif np.any(summed_spikes_per_bin[bin] == 0 for bin in range(len(summed_spikes_per_bin))) == True:
+            # If there is not a spike in every bin
+            dlpfc_no_spikes_in_a_bin_monkey.append(unit)
             
         elif np.sum(summed_spikes_per_bin) < 1:
             

Hunt - monkey frontal cortex/hunt_vmpfc.py
@@ -30,6 +30,8 @@ vmpfc_failed_fits = []
 vmpfc_no_autocorr_monkey = []
 vmpfc_no_spikes_in_a_bin_monkey = []
 vmpfc_low_fr_monkey = []
+
+vmpfc_avg_fr = []
         
 binsize = 50
 
@@ -51,9 +53,10 @@ for unit in range(len(spikes)):
             binned_unit_spikes.append(trial_spikes)
         binned_unit_spikes = np.array(binned_unit_spikes)
         binned_unit_spikes = binned_unit_spikes[:,:-1]
-
-        
+       
         summed_spikes_per_bin = np.sum(binned_unit_spikes,axis=0)
+        
+        vmpfc_avg_fr.append(np.sum(summed_spikes_per_bin)/rows)
         
         #%% Do autocorrelation
         
@@ -74,9 +77,9 @@ for unit in range(len(spikes)):
             
             vmpfc_no_autocorr_monkey.append(unit)
             
-        # elif [summed_spikes_per_bin[bin] == 0 for bin in range(len(summed_spikes_per_bin))]:
-            
-        #     vmpfc_no_spikes_in_a_bin_monkey.append(unit)
+        elif np.any(summed_spikes_per_bin[bin] == 0 for bin in range(len(summed_spikes_per_bin))) == True:
+            # If there is not a spike in every bin
+            vmpfc_no_spikes_in_a_bin_monkey.append(unit)
             
         elif np.sum(summed_spikes_per_bin) < 1:
             
@@ -256,57 +259,3 @@ plt.xlabel('tau')
 plt.ylabel('count')
 plt.title('%i monkey vmpfc units' %len(vmpfc_taus_monkey))
 plt.show()
-
-#%% How many units show initial incresae vs decrease
-
-# first_diff_vmpfc = []
-# second_diff_vmpfc = []
-
-# units = np.size(all_means_vmpfc_monkey,0)
-
-# for unit in range(units):
-    
-#     first_diff_vmpfc.append(all_means_vmpfc_monkey[unit,0]-all_means_vmpfc_monkey[unit,1])
-#     second_diff_vmpfc.append(all_means_vmpfc_monkey[unit,1]-all_means_vmpfc_monkey[unit,2])
-    
-# plt.hist(first_diff_vmpfc,label='first diff')
-# plt.hist(second_diff_vmpfc,label='second diff')
-# plt.ylabel('count')
-# plt.title('vmpfc')
-# plt.legend()
-# plt.show()
-
-#%% Plot autocorrelation curves for units with a positive vs negative difference between first and second lags
-
-# pos_first_diff = []
-# neg_first_diff = []
-
-# fig,axs = plt.subplots(1,2,sharey=True)
-
-# for unit in range(units):
-    
-#     if first_diff_vmpfc[unit] <= 0:
-        
-#         neg_first_diff.append(unit)
-        
-#     else:
-        
-#         pos_first_diff.append(unit)
-        
-# for dec_unit in range(len(neg_first_diff)):
-    
-#     axs[0].plot(x_m,all_means_vmpfc_monkey[neg_first_diff[dec_unit]])
-    
-# axs[0].set_title('%i vmpfc units with \n initial increase' %len(neg_first_diff))
-# axs[0].set_ylabel('autocorrelation')
-# axs[0].set_xlabel('lag (ms)')
-
-# for inc_unit in range(len(pos_first_diff)):
-    
-#    axs[1].plot(x_m,all_means_vmpfc_monkey[pos_first_diff[inc_unit]])
-    
-# axs[1].set_title('%i vmpfc units with \n initial decrease' % len(pos_first_diff))
-
-# plt.xlabel('lag (ms)')
-# plt.tight_layout()
-# plt.show()

Minxha - Human MFC/human_amygdala_all_plots.py
@@ -28,9 +28,11 @@ amyg_taus_m = []
 amyg_failed_fits = []
 
 
-failed_autocorr = []
-no_spikes_in_a_bin = []
-low_fr = []
+amyg_failed_autocorr = []
+amyg_no_spikes_in_a_bin = []
+amyg_low_fr = []
+
+amyg_avg_fr = []
 
 for unit in range(len(spikes)):
 
@@ -50,13 +52,15 @@ for unit in range(len(spikes)):
             
         binned_unit_spikes = np.vstack(binned_unit_spikes)
         
+        [trials,bins] = binned_unit_spikes.shape
+        
         summed_spikes_per_bin = np.sum(binned_unit_spikes,axis=0)
+        
+        amyg_avg_fr.append(np.sum(summed_spikes_per_bin)/trials)
         
         #%% Do autocorrelation
         
         one_autocorrelation = []
-        
-        [trials,bins] = binned_unit_spikes.shape
         
         for i in range(bins):
             for j in range(bins):
@@ -69,15 +73,15 @@ for unit in range(len(spikes)):
                 
         if np.isnan(one_autocorrelation).any() == True:
             
-            failed_autocorr.append(unit) # skip this unit if any autocorrelation fails
+            amyg_failed_autocorr.append(unit) # skip this unit if any autocorrelation fails
         
-        elif [summed_spikes_per_bin[bin] == 0 for bin in range(len(summed_spikes_per_bin))]:
-            
-            no_spikes_in_a_bin.append(unit) # skip this unit if any bin doesn't have spikes
+        elif np.any(summed_spikes_per_bin[bin] == 0 for bin in range(len(summed_spikes_per_bin))) == True:
+            # If there is not a spike in every bin
+            amyg_no_spikes_in_a_bin.append(unit)
         
-        elif np.sum(summed_spikes_per_bin) < 1:
+        elif np.sum(summed_spikes_per_bin)/trials < 1:
             
-            low_fr.append(unit) # skip this unit if avg firing rate across all trials is < 1
+            amyg_low_fr.append(unit) # skip this unit if avg firing rate across all trials is < 1
         
         else:              
                 
@@ -200,7 +204,7 @@ for unit in range(len(spikes)):
                     
 #%% How many units got filtered?
 
-bad_units = len(failed_autocorr) + len(no_spikes_in_a_bin) + len(low_fr)
+bad_units = len(amyg_failed_autocorr) + len(amyg_no_spikes_in_a_bin) + len(amyg_low_fr)
 
 print('%i units were filtered out' %bad_units)
 print('out of %i total units' %len(spikes))

Minxha - Human MFC/human_dacc_all_plots.py
@@ -29,6 +29,10 @@ dacc_no_autocorr = []
 dacc_no_spikes_in_a_bin = []
 dacc_low_fr = []
 
+dacc_avg_fr = []
+
+all_binned_spikes = []
+
 for unit in range(len(spikes)):
 
         this_unit = spikes[unit]
@@ -47,7 +51,11 @@ for unit in range(len(spikes)):
             
         binned_unit_spikes = np.vstack(binned_unit_spikes)
         
+        [trials,bins] = binned_unit_spikes.shape
+        
         summed_spikes_per_bin = np.sum(binned_unit_spikes,axis=0)
+        
+        dacc_avg_fr.append(np.sum(summed_spikes_per_bin)/trials)
         
         #%% Do autocorrelation
         
@@ -67,12 +75,12 @@ for unit in range(len(spikes)):
         if np.isnan(one_autocorrelation).any() == True:
             # If any autocorrelation fails (nan), skip this unit
             dacc_no_autocorr.append(unit)
-            
-        # elif [summed_spikes_per_bin[bin] == 0 for bin in range(len(summed_spikes_per_bin))]:
+  
+        elif np.any(summed_spikes_per_bin[bin] == 0 for bin in range(len(summed_spikes_per_bin))) == True:
             # If there is not a spike in every bin
-        #     dacc_no_spikes_in_a_bin.append(unit)
+            dacc_no_spikes_in_a_bin.append(unit)
             
-        elif np.sum(summed_spikes_per_bin) < 1:
+        elif np.sum(summed_spikes_per_bin)/trials < 1:
             # If firing rate < 1hz
             dacc_low_fr.append(unit)
         
@@ -186,11 +194,13 @@ for unit in range(len(spikes)):
                     
             first_neg_diff = np.min(neg_diffs)
             
+            first_neg_diff = int(first_neg_diff)
+            
             def func(x,a,tau,b):
                 return a*((np.exp(-x/tau))+b)
 
             try:
-                pars,cov = curve_fit(func,x_m[first_neg_diff:],y_m[first_neg_diff:],p0=[1,100,1],bounds=((0,np.inf)),maxfev=5000)
+                pars,cov = curve_fit(func,x_m,y_m,p0=[1,100,1],bounds=((0,np.inf)),maxfev=5000)
                 
             except RuntimeError:
                 print("Error - curve_fit failed")
@@ -200,13 +210,13 @@ for unit in range(len(spikes)):
             
             all_means_dacc.append(y_m)
             
-            plt.plot(x_m,y_m,'ro',label='original data')
-            plt.plot(x_m[first_neg_diff:],func(x_m[first_neg_diff:],*pars),label='fit')
-            plt.xlabel('lag (ms)')
-            plt.ylabel('mean autocorrelation')
-            plt.title('Human dACC %i' %unit)
-            plt.legend()
-            plt.show()
+            # plt.plot(x_m,y_m,'ro',label='original data')
+            # plt.plot(x_m[first_neg_diff:],func(x_m[first_neg_diff:],*pars),label='fit')
+            # plt.xlabel('lag (ms)')
+            # plt.ylabel('mean autocorrelation')
+            # plt.title('Human dACC %i' %unit)
+            # plt.legend()
+            # plt.show()
         
         
 #%% How many units didn't fit?

Minxha - Human MFC/human_preSMA_all_plots.py
@@ -26,9 +26,11 @@ spikes = presma['spikes']
 all_means_presma = []
 presma_taus = []
 
-failed_autocorr = []
-no_spikes_in_a_bin = []
-low_fr = []
+presma_failed_autocorr = []
+presma_no_spikes_in_a_bin = []
+presma_low_fr = []
+
+presma_avg_fr = []
 
 for unit in range(len(spikes)):
 
@@ -48,11 +50,15 @@ for unit in range(len(spikes)):
             
         binned_unit_spikes = np.vstack(binned_unit_spikes)
         
+        [trials,bins] = binned_unit_spikes.shape
+        
+        summed_spikes_per_bin = np.sum(binned_unit_spikes,axis=0)
+        
+        presma_avg_fr.append(np.sum(summed_spikes_per_bin)/trials)
+        
         #%% Do autocorrelation
         
-        one_autocorrelation = []
-        
-        [trials,bins] = binned_unit_spikes.shape
+        one_autocorrelation = []    
         
         for i in range(bins):
             for j in range(bins):
@@ -65,15 +71,15 @@ for unit in range(len(spikes)):
                 
         if np.isnan(one_autocorrelation).any() == True:
             
-            failed_autocorr.append(unit) # skip this unit if any autocorrelation fails
+            presma_failed_autocorr.append(unit) # skip this unit if any autocorrelation fails
         
-        elif [summed_spikes_per_bin[bin] == 0 for bin in range(len(summed_spikes_per_bin))]:
-            
-            no_spikes_in_a_bin.append(unit) # skip this unit if any bin doesn't have spikes
+        elif np.any(summed_spikes_per_bin[bin] == 0 for bin in range(len(summed_spikes_per_bin))) == True:
+            # If there is not a spike in every bin
+            presma_no_spikes_in_a_bin.append(unit)
         
-        elif np.sum(summed_spikes_per_bin) < 1:
+        elif np.sum(summed_spikes_per_bin)/trials < 1:
             
-            low_fr.append(unit) # skip this unit if avg firing rate across all trials is < 1
+            presma_low_fr.append(unit) # skip this unit if avg firing rate across all trials is < 1
         
         else:
                 
@@ -193,7 +199,7 @@ for unit in range(len(spikes)):
                 
 #%% How many units got filtered?
 
-bad_units = len(failed_autocorr) + len(no_spikes_in_a_bin) + len(low_fr)
+bad_units = len(presma_failed_autocorr) + len(presma_no_spikes_in_a_bin) + len(presma_low_fr)
 
 print('%i units were filtered out' %bad_units)
 print('out of %i total units' %len(spikes))